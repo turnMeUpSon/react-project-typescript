077825a459ee6e87c5d5e4503380cc0a41199cf8
mille <milleryan2003@gmail.com>
2022-11-14 19:39:00 +0300

    start working with redux and data

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import FavouritesPage from "./pages/FavouritesPage";
 import HomePage from "./pages/HomePage";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />}></Route>
-      <Route path="/favourites" element={<FavouritesPage />}></Route>
-    </Routes>
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/favourites" element={<FavouritesPage />}></Route>
+      </Routes>
+    </>
   );
 }
 
